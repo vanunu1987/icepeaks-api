d2ecbbde5d0f7844e2a182c3f44accd5695bfc97
yairv@yad2.co.il <yairv@yad2.co.il>
2022-08-25 00:49:34 +0300

    jwt

--- src/index.js
+++ src/index.js
@@ -8,30 +8,35 @@ const config = require('./config/dev')
 const cors = require('cors')
 const { expressjwt: jwt } = require('express-jwt');
 const jwks = require('jwks-rsa');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 // const { default: mongoose } = require('mongoose')
 // const { default: mongoose } = require('mongoose')
-const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-3ibaojjd.us.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'https://icepeaks.link',
-  issuer: 'https://dev-3ibaojjd.us.auth0.com/',
-  algorithms: ['RS256']
-});
+// const jwtCheck = jwt({
+//     secret: jwks.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: 'https://dev-3ibaojjd.us.auth0.com/.well-known/jwks.json'
+//   }),
+//   audience: 'https://icepeaks.link',
+//   issuer: 'https://dev-3ibaojjd.us.auth0.com/',
+//   algorithms: ['RS256']
+// });
+const checkJwt = auth({
+    audience: 'https://gw.icepeaks.link',
+    issuerBaseURL: `https://dev-3ibaojjd.us.auth0.com/`,
+  });
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000','https://www.icepeaks.link'],
+    origin: ['http://localhost:3000','https://ccb5-46-117-129-194.ngrok.io'],
     credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use(jwtCheck);
+app.use(checkJwt);
 app.use('/', routes);
 
 const port = process.env.PORT || config.server.port;
